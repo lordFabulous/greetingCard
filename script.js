73878fbde70c113af2d8a8d390404a602d02a43d
tijana <tijana.klimovic97@gmail.com>
2026-07-05 22:35:13 +0100

    Add Miki's present

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import confetti from 'https://cdn.jsdelivr.net/npm/canvas-confetti@1.6.0/+esm';
 
 function setupDynamicCard() {
     const urlParams = new URLSearchParams(window.location.search);
-    
+
     const imageType = urlParams.get('type');
     const triggerImage = document.getElementById('trigger-image');
     if (imageType === 'gift') {
@@ -10,16 +10,11 @@ function setupDynamicCard() {
     } else {
         triggerImage.src = './images/envelope-svgrepo-com.svg'; // The default fallback
     }
-
     const personName = urlParams.get('name');
     const titleElement = document.getElementById('birthday-title');
-    if (personName) {
-        // Capitalize the first letter just in case it was typed in lowercase
-        const formattedName = personName.charAt(0).toUpperCase() + personName.slice(1);
-        titleElement.textContent = `Happy Birthday, ${formattedName}!`;
-    } else {
-        titleElement.textContent = "Happy Birthday!";
-    }
+    const formattedName = personName ? personName.charAt(0).toUpperCase() + personName.slice(1) : "";
+    titleElement.textContent = `Happy Birthday ${formattedName}!`;
+    return formattedName;
 }
 
 function fireConfetti() {
@@ -31,29 +26,29 @@ function fireConfetti() {
     });
 }
 
-function revealSurprise() {
+function revealSurprise(name) {
     const instructionArrow = document.getElementById('instruction-arrow');
     const triggerContainer = document.getElementById('trigger-container');
-    const cakeContainer = document.getElementById('cake-container');
-
     instructionArrow.classList.add('hidden');
     triggerContainer.classList.add('hidden');
-    cakeContainer.classList.remove('hidden');
-    cakeContainer.classList.add('fade-in');
+
+    const giftContainer = name == "Miki" ? document.getElementById('present-for-miki') : document.getElementById('cake-container');
+    giftContainer.classList.remove('hidden');
+    giftContainer.classList.add('fade-in');
 }
 
-function handleTriggerClick() {
+function handleTriggerClick(name) {
     fireConfetti();
-    revealSurprise();
+    revealSurprise(name);
 }
 
 function init() {
     // 1. Set the correct image before the user sees it
-    setupDynamicCard();
+    const name = setupDynamicCard();
 
     // 2. Wait for the user to click
     const triggerContainer = document.getElementById('trigger-container');
-    triggerContainer.addEventListener('click', handleTriggerClick);
+    triggerContainer.addEventListener('click', () => { handleTriggerClick(name) });
 }
 
 init();
